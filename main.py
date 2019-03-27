5531f33c1e2c9cc7d6b1947db1cd3670896d71b8
mass2010chromium <jcpeng24@gmail.com>
2019-03-27 16:00:47 -0700

    Update main.py
    Tried making it take the best sentence from each paragraph - failed
    
    Tried limiting it to only one sentence from each paragraph - didn't change anything

--- main.py
+++ main.py
@@ -31,10 +31,22 @@ def summarize(text):
     # sent_tokenize breaks target text into sentences.
     blocks = [nltk.sent_tokenize(paragraph) for paragraph in text.split("\n")]
     sentence_list = []
-    for block in blocks:
+    summaries = []
+    r = Rake(min_length=2)
+    for i in range(len(blocks)):
+        block = blocks[i]
+        block = [[sentence, i] for sentence in block]
+        # if (len(block) > 1):
+        #     r.extract_keywords_from_text(" ".join(block))
+        #     keyphrases = [list(x) for x in r.get_ranked_phrases_with_scores()[:1]]
+        #     for sentence in block:
+        #         for phrase in keyphrases:
+        #             if (phrase[1] in sentence.lower()):
+        #                 summaries.append(sentence)
         sentence_list.extend(block)
 
-    r = Rake(min_length=2)
+    #return "\n".join(summaries)
+
     r.extract_keywords_from_text(text)
     #temp_keywords = r.get_ranked_phrases_with_scores()[:10]
     #keywords = [x[1] for x in temp_keywords]
@@ -45,23 +57,31 @@ def summarize(text):
 
     print()
 
-    important_sentences = [sentence_list[0]]
+    # important_sentences = [sentence_list[0][0]]
+    important_sentences = []
+
+    taken_paragraphs = []
 
     #prev_sentence = ""
     for i in sentence_list:
         is_important = False
         for j in keyphrases:
             for word in j[1:]:
-                if (word in i.lower()):
+                if (word in i[0].lower()):
                     is_important = True
                     break
         #if is_important and prev!= None and i not in important_sentences:
         #    important_sentences.append(prev + "\n" + i)
         #prev = i
-        if is_important and not (i in important_sentences):
+        if is_important and not (i[1] in taken_paragraphs) and not (i in important_sentences):
             important_sentences.append(i)
+            taken_paragraphs.append(i[1])
 
-    return "\n".join(important_sentences)
+    if not (sentence_list[0] in important_sentences):
+        important_sentences.append(sentence_list[0])
+
+
+    return "\n".join([sent[0] for sent in important_sentences])
 
 article = getFile(0).text
 print(article)
